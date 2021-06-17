acb79998201c9f639b70a79c6920adb99e5320da
ksfallon <fallon.katelin@gmail.com>
2021-06-17 16:40:29 -0400

    added my first prompt array  to trackerEmpCode.js

--- trackerEmpCode.js
+++ trackerEmpCode.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -16,3 +18,82 @@ connection.connect((err) => {
   if (err) throw err;
   runSearch();
 });
+
+const runEmpSearch = () => {
+  inquirer.prompt({
+    name: 'action',
+    type: 'list',
+    message: '', 
+    choices: [
+      {
+        name: "View All Employees",
+        value: "VIEW_EMPLOYEES"
+      },
+      {
+        name: "View All Employees By Department",
+        value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+      },
+      {
+        name: "View All Employees By Manager",
+        value: "VIEW_EMPLOYEES_BY_MANAGER"
+      },
+      {
+        name: "Add Employee",
+        value: "ADD_EMPLOYEE"
+      },
+      {
+        name: "Remove Employee",
+        value: "REMOVE_EMPLOYEE"
+      },
+      {
+        name: "Update Employee Role",
+        value: "UPDATE_EMPLOYEE_ROLE"
+      },
+      {
+        name: "Update Employee Manager",
+        value: "UPDATE_EMPLOYEE_MANAGER"
+      },
+      {
+        name: "View All Roles",
+        value: "VIEW_ROLES"
+      },
+      {
+        name: "Add Role",
+        value: "ADD_ROLE"
+      },
+      {
+        name: "Remove Role",
+        value: "REMOVE_ROLE"
+      },
+      {
+        name: "View All Departments",
+        value: "VIEW_DEPARTMENTS"
+      },
+      {
+        name: "Add Department",
+        value: "ADD_DEPARTMENT"
+      },
+      {
+        name: "Remove Department",
+        value: "REMOVE_DEPARTMENT"
+      },
+      {
+        name: "Quit",
+        value: "QUIT"
+      }
+    ],
+  })
+}
+
+// PROMPT SHOULD ALLOW USER TO DO AT LEAST:
+// 1 - ADD departments ROLES and EMPLOYEES
+// 2 - VIEW departments ROLES and EMPLOYEES
+// 3 - UPDATE employee ROLES 
+
+
+
+// BONUS
+// UPDATE employee MANAGERS 
+// VIEW employees by MANAGER
+// DELETE departments, roles, and employees
+// VIEW the total utililized budget of department (combined salaries of all employee in department)
