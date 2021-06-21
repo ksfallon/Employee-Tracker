d8b1f1d6db94d24408f5ba90120fad23e8dd9fdb
ksfallon <fallon.katelin@gmail.com>
2021-06-20 23:00:51 -0400

    did a test function to make sure I am connected to the mysql work bench database emp_trackerDB and it definitely is, just now need to figure out why my table isn't showing up in View Employees

--- server.js
+++ server.js
@@ -18,147 +18,159 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) throw err;
-  viewAndManage();
+  // viewAndManage();
 });
 
-const viewAndManage = () => {
-  inquirer.prompt({
-    type: 'list',
-    name: 'action',
-    message: 'What would you like to do?', 
-    choices: [
-      {
-        name: "View All Employees",
-        value: "VIEW_EMPLOYEES"
-      },
-      {
-        name: "View All Employees By Department",
-        value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
-      },
-      {
-        name: "View All Employees By Manager",
-        value: "VIEW_EMPLOYEES_BY_MANAGER"
-      },
-      {
-        name: "Add Employee",
-        value: "ADD_EMPLOYEE"
-      },
-      {
-        name: "Remove Employee",
-        value: "REMOVE_EMPLOYEE"
-      },
-      {
-        name: "Update Employee Role",
-        value: "UPDATE_EMPLOYEE_ROLE"
-      },
-      // ON DELETE SET NULL for manager id the rest or ON DELETE CASACADE**
-      {
-        name: "Update Employee Manager",
-        value: "UPDATE_EMPLOYEE_MANAGER"
-      },
-      {
-        name: "View All Roles",
-        value: "VIEW_ROLES"
-      },
-      {
-        name: "Add Role",
-        value: "ADD_ROLE"
-      },
-      {
-        name: "Remove Role",
-        value: "REMOVE_ROLE"
-      },
-      {
-        name: "View All Departments",
-        value: "VIEW_DEPARTMENTS"
-      },
-      {
-        name: "Add Department",
-        value: "ADD_DEPARTMENT"
-      },
-      {
-        name: "Remove Department",
-        value: "REMOVE_DEPARTMENT"
-      },
-      {
-        name: "Quit",
-        value: "QUIT"
-      }
-    ],
-  })
-  .then((answers) => {
-    switch (answers.action) {
-      case 'VIEW_EMPLOYEES':
-        employeesSearch();
-        break;
-      
-      case 'VIEW_EMPLOYEES_BY_DEPARTMENT':
-        employeesByDept();
-        break;
 
-      case 'VIEW_EMPLOYEES_BY_MANAGER':
-        employeesByManager();
-        break;
 
-      case 'ADD_EMPLOYEE':
-        addEmployee();
-        break;
-    
-      case 'REMOVE_EMPLOYEE':
-        removeEmployee();
-        break;
-
-      case 'UPDATE_EMPLOYEE_ROLE':
-        updateEmployeeRole();
-        break;
-        
-      case 'UPDATE_EMPLOYEE_MANAGER':
-        updateEmployeeManager();
-        break;
-        
-      case 'VIEW_ROLES':
-        viewRoles();
-        break;
-
-      case 'ADD_ROLES':
-        addRole();
-        break;
-
-      case 'REMOVE_ROLES':
-        removeRole();
-        break;
-
-      case 'VIEW_DEPARTMENTS':
-        viewDepartments();
-        break;
-
-      case 'ADD_DEPARTMENT':
-        addDepartment();
-        break;
-
-      case 'REMOVE_DEPARTMENT':
-        removeDepartment();
-        break;
-
-      case 'QUIT':
-        connection.end();
-        break;
-
-      default:
-        console.log(`Invalid action: ${answer.action}`);
-        break;
-      
-    }
-  })
-}
-
-const employeesSearch = () => {
-  connection.query(`SELECT employee.id AS "ID", employee.first_name AS "first_name", employee.last_name AS "last_name", role.title AS "title", department.name AS "department", role.salary AS "salary", CONCAT(manager.first_name, " ", manager.last_name) AS "manager" FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id ORDER BY id`), (err, result) => {
+const testFunction = () => {
+  const query = `SELECT * FROM employee WHERE employee.id = 1`
+  connection.query(query, (err, result) => {
     if (err) throw err;
-    console.table(result);
-    return viewAndManage();
-  };
+    console.table("Will i get CHUN? ", result)
+  })
 }
+
+testFunction()
+
+// const viewAndManage = () => {
+//   inquirer.prompt({
+//     type: 'list',
+//     name: 'action',
+//     message: 'What would you like to do?', 
+//     choices: [
+//       {
+//         name: "View All Employees",
+//         value: "VIEW_EMPLOYEES"
+//       },
+//       {
+//         name: "View All Employees By Department",
+//         value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+//       },
+//       {
+//         name: "View All Employees By Manager",
+//         value: "VIEW_EMPLOYEES_BY_MANAGER"
+//       },
+//       {
+//         name: "Add Employee",
+//         value: "ADD_EMPLOYEE"
+//       },
+//       {
+//         name: "Remove Employee",
+//         value: "REMOVE_EMPLOYEE"
+//       },
+//       {
+//         name: "Update Employee Role",
+//         value: "UPDATE_EMPLOYEE_ROLE"
+//       },
+//       // ON DELETE SET NULL for manager id the rest or ON DELETE CASACADE**
+//       {
+//         name: "Update Employee Manager",
+//         value: "UPDATE_EMPLOYEE_MANAGER"
+//       },
+//       {
+//         name: "View All Roles",
+//         value: "VIEW_ROLES"
+//       },
+//       {
+//         name: "Add Role",
+//         value: "ADD_ROLE"
+//       },
+//       {
+//         name: "Remove Role",
+//         value: "REMOVE_ROLE"
+//       },
+//       {
+//         name: "View All Departments",
+//         value: "VIEW_DEPARTMENTS"
+//       },
+//       {
+//         name: "Add Department",
+//         value: "ADD_DEPARTMENT"
+//       },
+//       {
+//         name: "Remove Department",
+//         value: "REMOVE_DEPARTMENT"
+//       },
+//       {
+//         name: "Quit",
+//         value: "QUIT"
+//       }
+//     ],
+//   })
+//   .then((answers) => {
+//     switch (answers.action) {
+//       case 'VIEW_EMPLOYEES':
+//         employeesSearch();
+//         break;
+      
+//       case 'VIEW_EMPLOYEES_BY_DEPARTMENT':
+//         employeesByDept();
+//         break;
+
+//       case 'VIEW_EMPLOYEES_BY_MANAGER':
+//         employeesByManager();
+//         break;
+
+//       case 'ADD_EMPLOYEE':
+//         addEmployee();
+//         break;
+    
+//       case 'REMOVE_EMPLOYEE':
+//         removeEmployee();
+//         break;
+
+//       case 'UPDATE_EMPLOYEE_ROLE':
+//         updateEmployeeRole();
+//         break;
+        
+//       case 'UPDATE_EMPLOYEE_MANAGER':
+//         updateEmployeeManager();
+//         break;
+        
+//       case 'VIEW_ROLES':
+//         viewRoles();
+//         break;
+
+//       case 'ADD_ROLES':
+//         addRole();
+//         break;
+
+//       case 'REMOVE_ROLES':
+//         removeRole();
+//         break;
+
+//       case 'VIEW_DEPARTMENTS':
+//         viewDepartments();
+//         break;
+
+//       case 'ADD_DEPARTMENT':
+//         addDepartment();
+//         break;
+
+//       case 'REMOVE_DEPARTMENT':
+//         removeDepartment();
+//         break;
+
+//       case 'QUIT':
+//         connection.end();
+//         break;
+
+//       default:
+//         console.log(`Invalid action: ${answer.action}`);
+//         break;
+      
+//     }
+//   })
+// }
+
+// const employeesSearch = () => {
+//   connection.query(`SELECT employee.id AS "ID", employee.first_name AS "first_name", employee.last_name AS "last_name", role.title AS "title", department.name AS "department", role.salary AS "salary", CONCAT(manager.first_name, " ", manager.last_name) AS "manager" FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id ORDER BY id`), (err, result) => {
+//     if (err) throw err;
+//     console.table(result);
+//     return viewAndManage();
+//   };
+// }
 
 
 const employeesByDept = (departmentId) => {
@@ -178,61 +190,64 @@ const employeesByManager = () => {
 
 const addEmployee = () => { 
 
-inquirer.prompt([
-  {
-    name: "first",
-    type: "input",
-    message: "What is the employee's first name?"
+// inquirer.prompt([
+//   {
+//     name: "first",
+//     type: "input",
+//     message: "What is the employee's first name?"
 
-  },
-  {
-    name: "last",
-    type: "input",
-    message: "What is the employee's last name?"
+//   },
+//   {
+//     name: "last",
+//     type: "input",
+//     message: "What is the employee's last name?"
 
-  },
-  {
-    name: "role",
-    type: "rawlist",
-    choices() {
-      const query = 'SELECT role.title FROM role'
-      connection.query(query, (err, result) => {
-        if (err) throw err;
-        let roleArr = [];
-        result.forEach(result_title => {
-          roleArr.push(result_title);
-        });
-        return roleArr;
-      })
-    },
-    message: "What is the employee's role?"
-  },
-  {
-    name: "manager",
-    type: "rawlist",
-    choices() {
-      const query = 'SELECT CONCAT(manager.first_name, " ", manager.last_name) AS "manager" FROM employee LEFT JOIN employee manager ON manager.id = employee.manager_id'
-      connection.query(query, (err, result) => {
-        if (err) throw err;
-        let managerArr = [];
-        result.forEach(result_manager => {
-          managerArr.push(result_manager);
-        })
-      })
-    },
-     message: "Who is the employee's manager?"
-  }
-]) // end of prompt section
-.then((answer) => {
-  connection.query(`INSERT into employee (first_name, last_name, role_id, manager_id) VALUE (first_name: ${answer.name}, last_name: ${anser.last}, role_id: SELECT role.id from role WHERE role.title = ${answer.role}, SELECT employee.id from employee WHERE concat(employee.first_name, " ", employee.last_name) = ${answer.manager})`, (err, result) => {
-    if (err) throw err;
-    console.log(result)
-    return viewAndManage();
-  })
+//   },
+//   {
+//     name: "role",
+//     type: "rawlist",
+//     choices() {
+//       const query = 'SELECT role.title FROM role'
+//       connection.query(query, (err, result) => {
+//         if (err) throw err;
+//         let roleArr = [];
+//         result.forEach(result_title => {
+//           roleArr.push(result_title);
+//         });
+//         return roleArr;
+//       })
+//     },
+//     message: "What is the employee's role?"
+//   },
+//   {
+//     name: "manager",
+//     type: "rawlist",
+//     choices() {
+//       const query = 'SELECT CONCAT(manager.first_name, " ", manager.last_name) AS "manager" FROM employee LEFT JOIN employee manager ON manager.id = employee.manager_id'
+//       connection.query(query, (err, result) => {
+//         if (err) throw err;
+//         let managerArr = [];
+//         result.forEach(result_manager => {
+//           managerArr.push(result_manager);
+//         })
+//       })
+//     },
+//      message: "Who is the employee's manager?"
+//   }
+// ]) 
+// end of prompt section
+// .then((answer) => {
+//   connection.query(`INSERT into employee (first_name, last_name, role_id, manager_id) VALUE (first_name: ${answer.name}, last_name: ${anser.last}, role_id: SELECT role.id from role WHERE role.title = ${answer.role}, SELECT employee.id from employee WHERE concat(employee.first_name, " ", employee.last_name) = ${answer.manager})`, (err, result) => {
+//     if (err) throw err;
+//     console.log(result)
+//     return viewAndManage();
+//   })
 
  
-}) //end of first then statement
-}; // end of the function
+// })
+ //end of first then statement
+}; 
+// end of the function
 
 
 
