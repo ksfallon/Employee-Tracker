fef9ca1d84a0a264fd32048b54210c58b8075007
ksfallon <fallon.katelin@gmail.com>
2021-06-20 23:44:18 -0400

    added functions VIEW EMPLOYEE VIEW ROLE and VIEW DEPARTMENT and they successfully post to the console in a table

--- server.js
+++ server.js
@@ -18,168 +18,185 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) throw err;
-  // viewAndManage();
+  viewAndManage();
 });
 
 
 
-const testFunction = () => {
-  const query = `SELECT * FROM employee WHERE employee.id = 1`
+// const testFunction = () => {
+//   const query = `SELECT * FROM employee WHERE employee.id = 1`
+//   connection.query(query, (err, result) => {
+//     if (err) throw err;
+//     console.table("Will i get CHUN? ", result)
+//   })
+// }
+
+// testFunction()
+
+const viewAndManage = () => {
+  inquirer.prompt({
+    type: 'list',
+    name: 'action',
+    message: 'What would you like to do?', 
+    choices: [
+      {
+        name: "View All Employees",
+        value: "VIEW_EMPLOYEES"
+      },
+      {
+        name: "View All Employees By Department",
+        value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+      },
+      {
+        name: "View All Employees By Manager",
+        value: "VIEW_EMPLOYEES_BY_MANAGER"
+      },
+      {
+        name: "Add Employee",
+        value: "ADD_EMPLOYEE"
+      },
+      {
+        name: "Remove Employee",
+        value: "REMOVE_EMPLOYEE"
+      },
+      {
+        name: "Update Employee Role",
+        value: "UPDATE_EMPLOYEE_ROLE"
+      },
+      
+      {
+        name: "Update Employee Manager",
+        value: "UPDATE_EMPLOYEE_MANAGER"
+      },
+      {
+        name: "View All Roles",
+        value: "VIEW_ROLES"
+      },
+      {
+        name: "Add Role",
+        value: "ADD_ROLE"
+      },
+      {
+        name: "Remove Role",
+        value: "REMOVE_ROLE"
+      },
+      {
+        name: "View All Departments",
+        value: "VIEW_DEPARTMENTS"
+      },
+      {
+        name: "Add Department",
+        value: "ADD_DEPARTMENT"
+      },
+      {
+        name: "Remove Department",
+        value: "REMOVE_DEPARTMENT"
+      },
+      {
+        name: "Quit",
+        value: "QUIT"
+      }
+    ],
+  })
+  .then((answers) => {
+    switch (answers.action) {
+      case 'VIEW_EMPLOYEES':
+        employeesSearch();
+        break;
+      
+      case 'VIEW_EMPLOYEES_BY_DEPARTMENT':
+        employeesByDept();
+        break;
+
+      case 'VIEW_EMPLOYEES_BY_MANAGER':
+        employeesByManager();
+        break;
+
+      case 'ADD_EMPLOYEE':
+        addEmployee();
+        break;
+    
+      case 'REMOVE_EMPLOYEE':
+        removeEmployee();
+        break;
+
+      case 'UPDATE_EMPLOYEE_ROLE':
+        updateEmployeeRole();
+        break;
+        
+      case 'UPDATE_EMPLOYEE_MANAGER':
+        updateEmployeeManager();
+        break;
+        
+      case 'VIEW_ROLES':
+        viewRoles();
+        break;
+
+      case 'ADD_ROLES':
+        addRole();
+        break;
+
+      case 'REMOVE_ROLES':
+        removeRole();
+        break;
+
+      case 'VIEW_DEPARTMENTS':
+        viewDepartments();
+        break;
+
+      case 'ADD_DEPARTMENT':
+        addDepartment();
+        break;
+
+      case 'REMOVE_DEPARTMENT':
+        removeDepartment();
+        break;
+
+      case 'QUIT':
+        connection.end();
+        break;
+
+      default:
+        console.log(`Invalid action: ${answer.action}`);
+        break;
+      
+    }
+  })
+};
+
+const employeesSearch = () => {
+  const query = `SELECT employee.id AS "id", employee.first_name AS "first name", employee.last_name AS "last name", role.title AS "title", department.name AS "department", role.salary AS "salary", CONCAT(manager.first_name, " ", manager.last_name) AS "manager" FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id ORDER BY id`
   connection.query(query, (err, result) => {
     if (err) throw err;
-    console.table("Will i get CHUN? ", result)
-  })
+    console.table(result)
+    return viewAndManage();
+  });
 }
 
-testFunction()
 
-// const viewAndManage = () => {
-//   inquirer.prompt({
-//     type: 'list',
-//     name: 'action',
-//     message: 'What would you like to do?', 
-//     choices: [
-//       {
-//         name: "View All Employees",
-//         value: "VIEW_EMPLOYEES"
-//       },
-//       {
-//         name: "View All Employees By Department",
-//         value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
-//       },
-//       {
-//         name: "View All Employees By Manager",
-//         value: "VIEW_EMPLOYEES_BY_MANAGER"
-//       },
-//       {
-//         name: "Add Employee",
-//         value: "ADD_EMPLOYEE"
-//       },
-//       {
-//         name: "Remove Employee",
-//         value: "REMOVE_EMPLOYEE"
-//       },
-//       {
-//         name: "Update Employee Role",
-//         value: "UPDATE_EMPLOYEE_ROLE"
-//       },
-//       // ON DELETE SET NULL for manager id the rest or ON DELETE CASACADE**
-//       {
-//         name: "Update Employee Manager",
-//         value: "UPDATE_EMPLOYEE_MANAGER"
-//       },
-//       {
-//         name: "View All Roles",
-//         value: "VIEW_ROLES"
-//       },
-//       {
-//         name: "Add Role",
-//         value: "ADD_ROLE"
-//       },
-//       {
-//         name: "Remove Role",
-//         value: "REMOVE_ROLE"
-//       },
-//       {
-//         name: "View All Departments",
-//         value: "VIEW_DEPARTMENTS"
-//       },
-//       {
-//         name: "Add Department",
-//         value: "ADD_DEPARTMENT"
-//       },
-//       {
-//         name: "Remove Department",
-//         value: "REMOVE_DEPARTMENT"
-//       },
-//       {
-//         name: "Quit",
-//         value: "QUIT"
-//       }
-//     ],
-//   })
-//   .then((answers) => {
-//     switch (answers.action) {
-//       case 'VIEW_EMPLOYEES':
-//         employeesSearch();
-//         break;
-      
-//       case 'VIEW_EMPLOYEES_BY_DEPARTMENT':
-//         employeesByDept();
-//         break;
-
-//       case 'VIEW_EMPLOYEES_BY_MANAGER':
-//         employeesByManager();
-//         break;
-
-//       case 'ADD_EMPLOYEE':
-//         addEmployee();
-//         break;
-    
-//       case 'REMOVE_EMPLOYEE':
-//         removeEmployee();
-//         break;
-
-//       case 'UPDATE_EMPLOYEE_ROLE':
-//         updateEmployeeRole();
-//         break;
-        
-//       case 'UPDATE_EMPLOYEE_MANAGER':
-//         updateEmployeeManager();
-//         break;
-        
-//       case 'VIEW_ROLES':
-//         viewRoles();
-//         break;
-
-//       case 'ADD_ROLES':
-//         addRole();
-//         break;
-
-//       case 'REMOVE_ROLES':
-//         removeRole();
-//         break;
-
-//       case 'VIEW_DEPARTMENTS':
-//         viewDepartments();
-//         break;
-
-//       case 'ADD_DEPARTMENT':
-//         addDepartment();
-//         break;
-
-//       case 'REMOVE_DEPARTMENT':
-//         removeDepartment();
-//         break;
-
-//       case 'QUIT':
-//         connection.end();
-//         break;
-
-//       default:
-//         console.log(`Invalid action: ${answer.action}`);
-//         break;
-      
-//     }
-//   })
-// }
-
-// const employeesSearch = () => {
-//   connection.query(`SELECT employee.id AS "ID", employee.first_name AS "first_name", employee.last_name AS "last_name", role.title AS "title", department.name AS "department", role.salary AS "salary", CONCAT(manager.first_name, " ", manager.last_name) AS "manager" FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id ORDER BY id`), (err, result) => {
-//     if (err) throw err;
-//     console.table(result);
-//     return viewAndManage();
-//   };
-// }
-
-
-const employeesByDept = (departmentId) => {
+const employeesByDept = (department) => {
   // const viewDepartments(departments)
-  // inquirer.prompt({
-  //   type: 'list',
-  //   name: 'departments',
-  //   message: "Which department do you want"
-  // })
+  inquirer.prompt({
+    type: 'rawlist',
+    name: 'departments',
+    choices() {
+      const choiceArray = [];
+      viewDepartments(department).forEach(department => {
+        choiceArray.push(department);
+      });
+      return choiceArray;
+
+      },
+      message: "Which department do you want",
+  })
+  .then((answer) => {
+    const query = `SELECT employee.first_name AS "First Name", employee.last_name AS "Last Name", department.name AS "Deparment" FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id WHERE department.name = ${answer.departments}`
+    connection.query(query, (err, result) => {
+      if (err) throw err;
+      console.table(result)
+      return viewAndManage();
+    });
+  })
   // connection.query('SELECT first_name AS Name, last_name, role_id FROM employees LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id WHERE department.id = ?', department.id (err, result) => {
   //   if (err) throw err;
 };
@@ -264,7 +281,12 @@ const updateEmployeeManager  = () => {
 }
 
 const viewRoles  = () => {
-
+  const query = `SELECT role.title AS "role title" FROM role ORDER BY role.id`
+  connection.query(query, (err, result) => {
+    if (err) throw err;
+    console.table(result)
+    return viewAndManage();
+  });
 }
 
 const addRole  = () => {
@@ -276,7 +298,12 @@ const removeRole  = () => {
 }
 
 const viewDepartments = () => {
-
+  const query = `SELECT department.name AS "department" FROM department ORDER BY department.id`
+  connection.query(query, (err, result) => {
+    if (err) throw err;
+    console.table(result)
+    return viewAndManage();
+  });
 }
 
 const addDepartment = () => {
